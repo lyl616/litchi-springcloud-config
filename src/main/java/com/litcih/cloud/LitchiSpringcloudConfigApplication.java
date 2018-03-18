703b02fe067ba894c343f72dead113e96c666f10
liuyulong <liuyulong@wamingtech.com>
2018-03-18 21:38:50 +0800

    config-server

---
+++ src/main/java/com/litcih/cloud/LitchiSpringcloudConfigApplication.java
@@ -0,0 +1,12 @@
+package com.litcih.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LitchiSpringcloudConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LitchiSpringcloudConfigApplication.class, args);
+	}
+}
---
+++ src/test/java/com/litcih/cloud/LitchiSpringcloudConfigApplicationTests.java
@@ -0,0 +1,16 @@
+package com.litcih.cloud;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LitchiSpringcloudConfigApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
